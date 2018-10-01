612d0c9568eb0a304d251144d04b260610e12143
Jose Alanya <jose.alanya@gmail.com>
2018-10-01 08:41:58 -0500

    [FEAT] - Documentation: Enhance HelloWorld with Comments

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import HelloWorld from '../../../components/HelloWorld/HelloWorld';
+
+/** Custome message */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Pluralsight viewers!" />
+}
