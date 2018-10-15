30ccd858042eb97e4d7fdfda2c5d4fc75af07c63
Jose Alanya <jose.alanya@gmail.com>
2018-10-15 07:53:45 -0500

    [FEAT] - Styling and Theming: CSS Modules

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -165,6 +165,8 @@ module.exports = {
                 loader: require.resolve('css-loader'),
                 options: {
                   importLoaders: 1,
+                  //modules: true,
+                  //localIdentName: '[name]_[local]_[hash:base64:5]'
                 },
               },
               {
